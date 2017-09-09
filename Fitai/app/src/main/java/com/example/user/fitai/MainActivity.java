1ad1ae9b81386658c9cefde154177124d1480230
kunalgarg2100 <kunalgarg2100@gmail.com>
2017-09-09 21:49:45 +0530

    Integrated Facebook and Google

--- Fitai/app/src/main/java/com/example/user/fitai/MainActivity.java
+++ Fitai/app/src/main/java/com/example/user/fitai/MainActivity.java
@@ -1,25 +1,44 @@
 package com.example.user.fitai;
 
-/**
- * Created by kunal on 27/8/17.
- */
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+
 public class MainActivity extends AppCompatActivity {
+
     CallbackManager callbackManager;
     LoginButton loginButton;
     TextView textView;
+
+    private SignInButton signinbutton;
+    private GoogleSignInOptions gso;
+    private GoogleApiClient mGoogleApiClient;
+    private int RC_SIGN_IN = 100;
+    //private TextView textViewName;
+    //private TextView textViewEmail;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,12 +70,73 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText("Login attempt failed.");
             }
         });
-    }
 
+        signinbutton = (SignInButton) findViewById(R.id.googlesigninbutton);
+        signinbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInWithGoogle();
+
+            }
+
+        });
+
+    }
+    private void signInWithGoogle() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        final Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleGoogleSignInResult(result);
+        }
+    }
+    //After the signing we are calling this function
+    private void handleGoogleSignInResult(GoogleSignInResult result) {
+        //If the login succeed
+        if (result.isSuccess()) {
+            //Getting google account
+            GoogleSignInAccount acct = result.getSignInAccount();
+            textView = (TextView)findViewById(R.id.textView);
+            textView.setText(
+                    "Login Sucess \n" +
+                            "User ID: " + acct.getDisplayName()
+                            + "\n" + "Email: " + acct.getEmail()
+            );
+
+
+            //Displaying name and email
+            //textViewName.setText(acct.getDisplayName());
+            //textViewEmail.setText(acct.getEmail());
+        } else {
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show();
+
+        }
+    }
+    private void GooglesignOut() {
+
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+
+                    }
+                });
     }
 
 
