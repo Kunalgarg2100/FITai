9a55059b25de8620e9386d4b774a9ec5d90bde17
mohit_123 <ms892075@gmail.com>
2017-09-25 20:37:29 +0530

    test project

---
+++ JavaMail/app/src/main/java/com/example/mohit/javamail/Config.java
@@ -0,0 +1,10 @@
+package com.example.mohit.javamail;
+
+/**
+ * Created by mohit on 24/9/17.
+ */
+
+public class Config {
+    public static final String EMAIL ="ms892075@gmail.com";
+    public static final String PASSWORD ="pranjaldon";
+}
