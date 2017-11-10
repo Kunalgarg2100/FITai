238021e8458ffbf7952aec9b320ce1067a938447
Nirvan Singhania <nirvan.singhania@students.iiit.ac.in>
2017-11-10 09:26:33 +0530

    Updated home page

--- Fitai/app/src/main/java/com/example/user/fitai/fragment/HomeFragment.java
+++ Fitai/app/src/main/java/com/example/user/fitai/fragment/HomeFragment.java
@@ -1,30 +1,20 @@
 package com.example.user.fitai.fragment;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.example.user.fitai.Dashboard;
-import com.example.user.fitai.LoginActivity;
 import com.example.user.fitai.R;
 import com.example.user.fitai.SetProfile;
 import com.example.user.fitai.adapter.Workout;
@@ -59,8 +49,7 @@ public class HomeFragment extends TabsFragment{
             }
         });
 
-        name = (TextView) view.findViewById(R.id.username);
-        name.setText("name :");
+
         BarChart chart = (BarChart) view.findViewById(R.id.chart);
         BarData data = new BarData(getXAxisValues(), getDataSet());
         chart.setData(data);
@@ -69,16 +58,7 @@ public class HomeFragment extends TabsFragment{
         chart.invalidate();
         Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
 
-        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
 
-        workoutList = new ArrayList<>();
-        adapter = new WorkoutsAdapter(getContext(), workoutList);
-
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getContext(), 1);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.addItemDecoration(new HomeFragment.GridSpacingItemDecoration(1, dpToPx(10), true));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(adapter);
 
         scdule = (TextView) view.findViewById(R.id.schedule);
         scdule.setOnClickListener(new View.OnClickListener() {
@@ -88,7 +68,6 @@ public class HomeFragment extends TabsFragment{
             }
         });
 
-        prepareWorkouts();
 
         try {
             Glide.with(this).load(R.drawable.cover).into((ImageView) view.findViewById(R.id.backdrop));
@@ -149,56 +128,5 @@ public class HomeFragment extends TabsFragment{
         xAxis.add("MAY");
         xAxis.add("JUN");
         return xAxis;
-    }
-
-    private void prepareWorkouts() {
-        int[] covers = new int[]{
-                R.drawable.pushup,
-        };
-
-        Workout a = new Workout("Pushups", "This is a must if you want to increase your strength", covers[0]);
-        workoutList.add(a);
-        adapter.notifyDataSetChanged();
-    }
-
-
-    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
-
-        private int spanCount;
-        private int spacing;
-        private boolean includeEdge;
-
-        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
-            this.spanCount = spanCount;
-            this.spacing = spacing;
-            this.includeEdge = includeEdge;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            int position = parent.getChildAdapterPosition(view); // item position
-            int column = position % spanCount; // item column
-
-            if (includeEdge) {
-                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
-                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
-
-                if (position < spanCount) { // top edge
-                    outRect.top = spacing;
-                }
-                outRect.bottom = spacing; // item bottom
-            } else {
-                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
-                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
-                if (position >= spanCount) {
-                    outRect.top = spacing; // item top
-                }
-            }
-        }
-    }
-
-    private int dpToPx(int dp) {
-        Resources r = getResources();
-        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 }
